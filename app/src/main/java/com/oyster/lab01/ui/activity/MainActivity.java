fe6d222eed403edba10ec6d3c6bc6ff6576bf1bc
Andriy Bas <avikbascra@gmail.com>
2014-10-12 10:15:43 +0300

    add lab2

--- app/src/main/java/com/oyster/lab01/ui/activity/MainActivity.java
+++ app/src/main/java/com/oyster/lab01/ui/activity/MainActivity.java
@@ -1,15 +1,10 @@
 package com.oyster.lab01.ui.activity;
 
-import android.app.Activity;
 import android.app.ActionBar;
-import android.app.Fragment;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.Toast;
 
 import com.oyster.lab01.R;
